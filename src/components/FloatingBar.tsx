34bd2e47a25a4d81e9f2f6e00c9031bc20210d9f
Alexandre Talles <alexandretalles777@gmail.com>
2023-03-21 14:37:54 -0300

    floating bar finished

--- src/components/FloatingBar.tsx
+++ src/components/FloatingBar.tsx
@@ -7,21 +7,21 @@ import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 export const FloatingBar = () => {
   return (
-    <nav className='fixed bottom-2 md:bottom-8 w-full overflow-hidden z-50'>
+    <nav className='fixed bottom-5 md:bottom-8 w-full overflow-hidden z-50'>
       <div className='container mx-auto'>
         <div className='w-[calc(100%-15px)] bg-black/20 h-16 backdrop-blur-2xl rounded-full 
         max-w-[450px] mx-auto flex justify-around items-center text-2xl text-white'>
 
-        <Link to='home' activeClass={'active'} className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
+        <Link to='home' className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
           <FontAwesomeIcon icon={faHome} />
         </Link>
-        <Link to='about' activeClass={'active'} className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
+        <Link to='about' className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
           <FontAwesomeIcon icon={faUserCircle} />
         </Link>
-        <Link to='work' activeClass={'active'} className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
+        <Link to='work' className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
           <FontAwesomeIcon icon={faBriefcase} />
         </Link>
-        <Link to='contact' activeClass={'active'} className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
+        <Link to='contact' className='cursor-pointer w-14 h-14 flex items-center justify-center rounded-full' activeClass='active' smooth={true} spy={true}>
           <FontAwesomeIcon icon={faPhone} />
         </Link>
 
